e5dee189f6fd6330f4fcaf2bc5f69d6633b9cb82
F4LK0N <otaviosoria@gmail.com>
2022-10-03 00:49:36 -0300

    Core Framework - Model Base

---
+++ src/core/Models/Model.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Core\Models;
+
+class Model
+{
+    static public $TEST = null;
+
+    protected $dbStructure = null;
+
+    public function __construct()
+    {
+//        var_dump($this);
+//        var_dump(get_class());
+//        var_dump(get_parent_class());
+//        var_dump(get_called_class());
+    }
+
+}
--- src/core/autoload.php
+++ src/core/autoload.php
@@ -1,3 +1,5 @@
 <?php
 
 require __DIR__."/Controllers/Controller.php";
+
+require __DIR__."/Models/Model.php";
