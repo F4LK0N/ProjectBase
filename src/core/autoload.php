a11910b370f12ee4176c13d94b175eee5220f3a9
F4LK0N <otaviosoria@gmail.com>
2022-10-27 02:48:52 -0300

    Core - Autoload

--- src/core/autoload.php
+++ src/core/autoload.php
@@ -1,7 +1,36 @@
 <?php
 
-require __DIR__."/DB/Table.php";
+use Slim\Factory\AppFactory;
 
-require __DIR__."/Controllers/Controller.php";
+//### AUTO LOADER ###
+// Autoload the core library code.
+// The files required in this file must be loaded in this exactly specific order.
 
-require __DIR__."/Models/Model.php";
+//Vendor
+require_once __DIR__.'/../vendor/autoload.php';
+
+
+
+//Basic
+require_once __DIR__."/Basic/HttpHeaders.php";
+require_once __DIR__."/Basic/eGTW_TYPE.php";
+//...
+
+//Helpers
+//...
+
+//DB
+require_once __DIR__."/DB/Table.php";
+//...
+
+//Controllers
+require_once __DIR__."/Controllers/Controller.php";
+//...
+
+//Models
+require_once __DIR__."/Models/Model.php";
+//...
+
+
+
+$GLOBALS['app'] = AppFactory::create();
