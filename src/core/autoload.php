bbb22bf5c9f6070237a21c9d0263010615097a99
F4LK0N <otaviosoria@gmail.com>
2022-10-28 23:50:30 -0300

    Core - Tests - Autoloader Remove

--- src/core/autoload.php
+++ src/core/autoload.php
@@ -1,20 +1,30 @@
-<?php
-
+<?php 
+/** @noinspection PhpDefineCanBeReplacedWithConstInspection */
 use Slim\Factory\AppFactory;
 
 //### AUTO LOADER ###
 // Autoload the core library code.
 // The files required in this file must be loaded in this exactly specific order.
 
-//Vendor
-require_once __DIR__.'/../vendor/autoload.php';
 
 
+//Enumerators
+require_once __DIR__ . "/Enumerations/HTTP_HEADER_CONTENT_TYPE.php";
+//... ENUM ...
+//... ENUM ...
+//... ENUM ...
+//...
 
 //Basic
-require_once __DIR__."/Basic/HttpHeaders.php";
-require_once __DIR__."/Basic/eGTW_TYPE.php";
+require_once __DIR__."/Basic/HTTP_HEADERS.php";
+//require_once __DIR__."/Basic/PATH.php";
+//require_once __DIR__."/Basic/GTW.php";
+//require_once __DIR__."/Basic/SANITIZATION.php";
+//require_once __DIR__."/Basic/SERVER.php";
 //...
+
+//Vendor (Third Party Libraries)
+require_once __DIR__.'/../vendor/autoload.php';
 
 //Helpers
 //...
